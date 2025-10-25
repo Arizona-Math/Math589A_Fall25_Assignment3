54a95c77ba1f8dc575ffd1e391025653b406baaf
Marek Rychlik <rychlik@arizona.edu>
2025-10-25 16:10:10 -0700

    *** empty log message ***

--- scripts/grade.m
+++ scripts/grade.m
@@ -1,7 +1,7 @@
 function grade
 % scripts/grade.m  -- compact, parseable output for autograder
-addpath('src');
-y = readmatrix('data/y_example.csv');
+addpath(fullfile('..','src'));
+y = readmatrix(fullfile('..','data','y_example.csv');
 s = 12; Ngrid = 0:10; Kgrid = 0:3; criterion = 'bic';
 best = select_model(y, s, Ngrid, Kgrid, criterion);
 yhat = predict_in_sample(y, s, best.coef);
