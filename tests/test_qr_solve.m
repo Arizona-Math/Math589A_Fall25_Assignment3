11c266819395854ad0a71d9ce601d5b2b94f437d
Marek Rychlik <rychlik@arizona.edu>
2025-10-25 20:17:53 -0700

    *** empty log message ***

--- tests/test_qr_solve.m
+++ tests/test_qr_solve.m
@@ -4,5 +4,5 @@ rng(0); A = randn(50,5); x0 = randn(5,1); b = A*x0 + 1e-8*randn(50,1);
 x = qr_solve_dense(A,b);
 res = norm(A*x-b);
 fprintf('QR_solve residual: %.3e\n', res);
-% Use assert so that criteria of passing are tested automatically
+% Use assert so that criteria of passing are tested automatically!
 assert(res < EPS)
