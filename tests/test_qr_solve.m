95261bbec67c31331dfde48417f4854d62ed0679
Marek Rychlik <rychlik@arizona.edu>
2025-10-25 20:16:34 -0700

    *** empty log message ***

--- tests/test_qr_solve.m
+++ tests/test_qr_solve.m
@@ -1,4 +1,7 @@
 addpath(fullfile('..','src'));
+EPS=1e-6;                               % Threshold for residual
 rng(0); A = randn(50,5); x0 = randn(5,1); b = A*x0 + 1e-8*randn(50,1);
 x = qr_solve_dense(A,b);
-fprintf('QR_solve residual: %.3e\n', norm(A*x-b));
+res = norm(A*x-b);
+fprintf('QR_solve residual: %.3e\n', res);
+assert(res < EPS)
